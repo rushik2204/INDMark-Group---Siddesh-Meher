b084c4aac12351a2eb8c0a2ea51b8468f49e030e
unknown <rushikeshk2210@gmail.com>
2026-05-07 11:43:05 +0530

    fix asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-// @lovable.dev/vite-tanstack-config already includes the following — do NOT add them manually
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   vite: {
+    base: "/",
     plugins: [
       netlify(),
     ],
